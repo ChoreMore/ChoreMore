cdd17a65a7cd2fb76f075049e6909f7a82f39530
Nicholas Cross <nicholas@nacrossweb.com>
2017-02-24 21:35:00 +1100

    Added a few styles to the app based on Kamals designs

--- ChoreMore/ChoreMore/Source/AppDelegate.swift
+++ ChoreMore/ChoreMore/Source/AppDelegate.swift
@@ -15,7 +15,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UINavigationBar.appearance().barTintColor = UIColor(colorLiteralRed: 52.0/255, green: 145.0/255, blue: 51.0/255, alpha: 1)
+        
+        UINavigationBar.appearance().titleTextAttributes = [
+             NSForegroundColorAttributeName : UIColor.white
+        ]
+        
+        UIBarButtonItem.appearance().setTitleTextAttributes(
+            [
+                NSForegroundColorAttributeName : UIColor.white
+            ], for: .normal)
+        
+        UINavigationBar.appearance().tintColor = UIColor.white
+
         return true
     }
 
