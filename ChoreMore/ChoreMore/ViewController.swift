44c48a35bd29fcea53f7346403ace7e198b69166
Nicholas Cross <nick.cross@outware.com.au>
2017-02-24 12:59:34 +1100

    Initial commit

---
+++ ChoreMore/ChoreMore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChoreMore
+//
+//  Created by Nicholas Cross on 24/02/2017.
+//  Copyright © 2017 Nicholas Cross. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
