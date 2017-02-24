0922ac5c8eb7bdd50fdddf3b1538f64de3cd2139
Nick <nicholas@Outwares-MBP.guest.ipc.telstra.net>
2017-02-24 14:39:39 +1100

    Added empty view controllers

---
+++ ChoreMore/ChoreMore/Source/Common/LoginViewController.swift
@@ -0,0 +1,11 @@
+//
+// Created by Nicholas Cross on 24/02/2017.
+// Copyright (c) 2017 Nicholas Cross. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginViewController : UIViewController {
+
+}
