bafebc3b61395dab16bf893deaf48b1dbe8d88c3
Nick <nicholas@Outwares-MBP.guest.ipc.telstra.net>
2017-02-24 17:18:56 +1100

    Added family member cell displaying children in view controller

---
+++ ChoreMore/ChoreMore/Source/Parent/FamilyMemberCell.swift
@@ -0,0 +1,14 @@
+//
+//  FamilyMemberCell.swift
+//  ChoreMore
+//
+//  Created by Nicholas Cross on 24/02/2017.
+//  Copyright © 2017 Nicholas Cross. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class FamilyMemberCell : UITableViewCell {
+    @IBOutlet weak var childNameLabel: UILabel!
+}
