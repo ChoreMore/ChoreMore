a7fb1a055e6a411280c0b7e4b051897979aefc95
James Litjens <james@jameslitjens.com>
2017-02-24 16:47:29 +1100

    Kid Chores List controller

--- ChoreMore/ChoreMore/Source/Child/ChoreListViewController.swift
+++ ChoreMore/ChoreMore/Source/Child/ChoreListViewController.swift
@@ -6,8 +6,41 @@
 import Foundation
 import UIKit
 
-class ChoreListViewController : UIViewController {
+class ChoreListViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var choreList: [String]?
     
     @IBOutlet weak var choresTableView: UITableView!
+    @IBOutlet weak var childNameLabel: UILabel!
+    @IBOutlet weak var childBalanceLabel: UILabel!
+    @IBOutlet weak var choreTotalAvailableLabel: UILabel!
+    
+    override func viewDidLoad() {
+        self.choreList = ["Clean dishes", "Make bed"]
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let count = self.choreList?.count else {
+            return 0
+        }
+        
+        return count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let choreCell = tableView.dequeueReusableCell(withIdentifier: "choreCell") as! ChoreTableViewCell
+        
+        guard let chore = self.choreList?[indexPath.item] else {
+            return UITableViewCell()
+        }
+        
+        choreCell.choreNameLabel.text = chore
+        
+        return choreCell
+    }
+    
+    @IBAction func unwindToParentChoreList(segue: UIStoryboardSegue) {
+        
+    }
 
 }
