9a32065a6f65ee9d09b9174e0ff86d354d8bac01
James Litjens <james@jameslitjens.com>
2017-02-24 19:22:31 +1100

    Wishlist screen

--- ChoreMore/ChoreMore/Source/Child/WishlistTableViewCell.swift
+++ ChoreMore/ChoreMore/Source/Child/WishlistTableViewCell.swift
@@ -12,11 +12,8 @@ class WishlistTableViewCell: UITableViewCell {
 
     @IBOutlet weak var itemNameLabel: UILabel!
     @IBOutlet weak var itemCostLabel: UILabel!
+    @IBOutlet weak var itemImage: UIImageView!
     
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
