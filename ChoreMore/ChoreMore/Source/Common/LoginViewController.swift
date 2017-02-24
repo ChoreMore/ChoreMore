aff33904cef82c11b555d29497dbfe5aac7d601f
Hadi <hadi@nab.com>
2017-02-24 22:22:41 +1100

    Add `AccountDetails` and `AccountBalance` APIs

--- ChoreMore/ChoreMore/Source/Common/LoginViewController.swift
+++ ChoreMore/ChoreMore/Source/Common/LoginViewController.swift
@@ -14,9 +14,22 @@ class LoginViewController : UIViewController {
   private var apiManager: APIManager?
 
   @IBAction func login(_ sender: Any) {
-    apiManager = APIManager()
-    apiManager?.login { state in
-    }
+//    apiManager = APIManager()
+//    apiManager?.login { state in
+//      print(state)
+//    }
+//
+//    apiManager?.accountDetails() { response in
+//      if let accountsResponse = response["accountsResponse"] as? [String: Any], let accounts = accountsResponse["accounts"] as? [Any] {
+//        for account in accounts {
+//          if let account = account as? [String: Any] {
+//            self.apiManager?.accountBalance(accountToken: account["accountToken"]! as! String) { balance in
+//              print(balance)
+//            }
+//          }
+//        }
+//      }
+//    }
   }
 
 }
