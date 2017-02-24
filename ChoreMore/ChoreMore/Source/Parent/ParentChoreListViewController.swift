d2768122c30eff0bc5cb17bf95277de9dfb10c4c
Nick <nicholas@Outwares-MBP.guest.ipc.telstra.net>
2017-02-24 15:37:04 +1100

    Updated parent chore list

--- ChoreMore/ChoreMore/Source/Parent/ParentChoreListViewController.swift
+++ ChoreMore/ChoreMore/Source/Parent/ParentChoreListViewController.swift
@@ -6,8 +6,26 @@
 import Foundation
 import UIKit
 
-class ParentChoreListViewController : UIViewController {
+class ParentChoreListViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    var choreList: [String]?
+
+    override func viewDidLoad() {
+        self.choreList = ["haha", "haha22"]
+    }
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        guard let count = self.choreList?.count else {
+            return 0
+        }
+        
+        return count
+    }
+
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return tableView.dequeueReusableCell(withIdentifier: "ParentChore")!
+    }
+    
     @IBAction func unwindToParentChoreList(segue: UIStoryboardSegue) {
 
     }
